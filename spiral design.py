426c28f3dcfbde6bed026be4ca31dc2431cbbf47
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 13:17:03 +0530

    Update spiral design.py

--- spiral design.py
+++ spiral design.py
@@ -1,8 +1,8 @@
 import turtle
 
-colors=["red","green","orange","purple","yellow","blue"]
+colors=["red","green","orange","purple","yellow","blue"]    # you can change colors for your wish
 t=turtle.Pen()
-t.speed(50)
+t.speed(50)             # it represents the drawing speed
 turtle.bgcolor("black")
 for i in range(1000):
     t.pencolor(colors[i%6])
